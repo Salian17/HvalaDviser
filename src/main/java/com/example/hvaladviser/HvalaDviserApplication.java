b54d690b4acf4322cc893de2df03624eebd14889
Salian17 <sazonov1744@yandex.ru>
2024-10-09 17:29:14 +0300

    init commit

---
+++ src/main/java/com/example/hvaladviser/HvalaDviserApplication.java
@@ -0,0 +1,13 @@
+package com.example.hvaladviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvalaDviserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvalaDviserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hvaladviser/HvalaDviserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hvaladviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvalaDviserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
