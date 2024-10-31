44ed77457515975b72bd505bae172b73339fe74f
Salian17 <sazonov1744@yandex.ru>
2024-10-31 10:33:48 +0300

    add validations and comments

--- src/main/java/com/example/hvaladviser/models/Restaurant.java
+++ src/main/java/com/example/hvaladviser/models/Restaurant.java
@@ -2,17 +2,36 @@ package com.example.hvaladviser.models;
 
 import com.example.hvaladviser.enums.AverageBillsEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * This class representing Restaurant model.
+ *
+ * @property id The id of restaurant.
+ * @property averageBillsEnum The average bill in restaurant.
+ * @property openHours The hours when restaurant opened.
+ *
+ * @author Salian17
+ */
 @Entity
 public class Restaurant {
     private UUID id;
     private AverageBillsEnum averageBillsEnum;
     private List<String> openHours;
 
-    protected Restaurant() {}
+    /**
+     * Protected constructor for JPA.
+     */
+    protected Restaurant() {/** do nothing*/}
+
+    public Restaurant(UUID id, AverageBillsEnum averageBillsEnum, List<String> openHours) {
+        this.id = id;
+        this.averageBillsEnum = averageBillsEnum;
+        this.openHours = openHours;
+    }
 
     @Id
     @GeneratedValue
@@ -25,6 +44,7 @@ public class Restaurant {
     }
 
     @Enumerated(EnumType.STRING)
+    @NotBlank(message = "Average bills required")
     public AverageBillsEnum getAverageBillsEnum() {
         return averageBillsEnum;
     }
@@ -34,6 +54,7 @@ public class Restaurant {
     }
 
     @ElementCollection
+    @NotBlank(message = "Open hours required")
     public List<String> getOpenHours() {
         return openHours;
     }
