98995f934cb2ef06d696fc2d74c2a4bcecc85f13
Salian17 <sazonov1744@yandex.ru>
2024-11-04 03:42:44 +0300

    edit restaurant repository

--- src/main/java/com/example/hvaladviser/repositories/RestaurantRepository.java
+++ src/main/java/com/example/hvaladviser/repositories/RestaurantRepository.java
@@ -1,9 +1,17 @@
 package com.example.hvaladviser.repositories;
 
+import com.example.hvaladviser.models.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * This class representing Restaurant repository
+ * with extends JpaRepository
+ *
+ * @author Xenia
+ */
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
 
